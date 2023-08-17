3b1a48e4b6292ca5a290b50e61509e19ca0caaa1
Your Namecl <you@example.com>
2023-08-17 15:42:59 +0200

    commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,19 +8,7 @@ import { Chat } from "./components/Chat";
 
 function App() {
   const { isLoggedIn } = useChatContext();
-  // const [username, setUsername] = useState("");
-  // const socket = io("http://localhost:3000", { autoConnect: false });
 
-  // useEffect(() => {
-  //   socket.on("new_user_connected", (username: string) => {
-  //     console.log(username);
-  //   });
-  // }, []);
-
-  // const initChat = () => {
-  //   socket.connect();
-  //   socket.emit("user_connected", username);
-  // };
   return <>{isLoggedIn ? <Chat /> : <Login />}</>;
 }
 
